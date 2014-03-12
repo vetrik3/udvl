64a895c9037993761fe6fd8f5eadd32e8acae680
Jozef Šiška <siska@ii.fmph.uniba.sk>
2014-03-12 14:55:41 +0100

    cv04: upraveny python test, aby 'bodoval' rovanko ako cpp
    + posledne 2 testy z cpp

--- cv04/cv04test.py
+++ cv04/cv04test.py
@@ -12,20 +12,21 @@ from sudoku import SudokuSolver
 
 class Tester(object):
     def __init__(self):
+        self.case = 0
         self.tested = 0
         self.passed = 0
 
+    def fail(self, msg):
+        print('ERROR: %s' % msg)
+        return False;
     def checkList(self, l, msg):
         if len(l) != 9:
-            print('ERROR: Wrong result format %s!' % (msg,))
-            return False
+            return self.fail('Wrong result format %s!' % (msg,))
         s = sorted(list(set(l)))
         if len(s) !=9:
-            print('ERROR: Duplicate number %s!' % (msg,))
-            return False
+            return self.fail('Duplicate number %s!' % (msg,))
         if s != list(range(1,10)):
-            print('ERROR: Wrong numbers %s!' % (msg,))
-            return False
+            return self.fail('Wrong numbers %s!' % (msg,))
         return True
 
     def checkInput(self, i, s):
@@ -33,35 +34,62 @@ class Tester(object):
             for c in range(9):
                 if i[r][c] != 0:
                     if i[r][c] != s[r][c]:
-                        print('ERROR: does not match input at %d,%d!' % (r,c))
-                        return False
+                        return self.fail('does not match input at %d,%d!' % (r,c))
         return True
 
     def checkGood(self, i, s):
-        if not self.checkInput(i, s):
-            return False
+        self.tested += 4
+        ret = True
+        if self.checkInput(i, s):
+            self.passed += 1
+        else:
+            ret = False
+
+        rret = True
         for row,r in zip(s,range(9)):
             if not self.checkList(row, 'in row %d: %s' % (r,repr(row))):
-                return False
+                rret = False
+                break
+        if rret:
+            self.passed += 1
+        else:
+            ret = False
+
+        rret = True
         for c in range(9):
             col = [ row[c] for row in s ]
             if not self.checkList(col, 'in col %d: %s' % (c,repr(col))):
-                return False
+                rret = False
+                break
+        if rret:
+            self.passed += 1
+        else:
+            ret = False
+
+        rret = True
         for sr in range(3):
             for sc in range(3):
                 a = sc*3
                 b = sc*3 + 3
                 l = s[sr*3][a:b] + s[sr*3+1][a:b] + s[sr*3+2][a:b]
                 if not self.checkList(l, 'in square %d,%d: %s' % (sr,sc,repr(l))):
-                    return False
-        return True
+                    rret = False
+                    break
+        if rret:
+            self.passed += 1
+        else:
+            ret = False
+
+        return ret
 
     def checkBad(self, s):
+        self.tested += 4
         for r in s:
             for c in r:
                 if c:
                     print('ERROR: Nonzero in bad sudoku!')
                     return False
+        self.passed += 4
         return True
 
     def check(self, i, good, s):
@@ -72,10 +100,9 @@ class Tester(object):
 
 
     def test(self, i, good, s):
-        self.tested += 1
-        sys.stdout.write('Case %d:  ' % (self.tested,))
+        self.case += 1
+        sys.stdout.write('Case %d:  ' % (self.case,))
         if self.check(i, good, s):
-            self.passed += 1
             print('PASSED')
         else:
             print('')
@@ -88,8 +115,8 @@ class Tester(object):
             print('')
 
     def status(self):
-        print("TESTED %d" % (self.tested,))
-        print("PASSED %d" % (self.passed,))
+        print("self.tested %d" % (self.tested,))
+        print("self.passed %d" % (self.passed,))
         if self.tested == self.passed:
             print("OK")
         else:
@@ -148,6 +175,33 @@ i = [
     [0, 0, 6, 0, 0, 0, 4, 0, 0],
     [7, 0, 0, 0, 0, 2, 5, 9, 3],
 ]
+t.test(i, True, SudokuSolver().solve(i))
+
+i = [
+    [5, 3, 4, 6, 7, 8, 9, 1, 0],
+    [6, 7, 2, 1, 9, 5, 3, 4, 0],
+    [1, 9, 8, 3, 4, 2, 5, 6, 0],
+    [8, 5, 9, 7, 6, 1, 4, 2, 0],
+    [4, 2, 6, 8, 5, 3, 7, 9, 0],
+    [7, 1, 3, 9, 2, 4, 8, 5, 0],
+    [9, 6, 1, 5, 3, 7, 2, 8, 0],
+    [2, 8, 7, 4, 1, 9, 6, 3, 0],
+    [3, 4, 5, 2, 8, 6, 1, 7, 0],
+]
+t.test(i, True, SudokuSolver().solve(i))
+
+i = [
+    [5, 3, 4, 6, 7, 8, 9, 1, 2],
+    [6, 7, 2, 1, 9, 5, 3, 4, 8],
+    [1, 9, 8, 3, 4, 2, 5, 6, 7],
+    [8, 5, 9, 7, 6, 1, 4, 2, 3],
+    [4, 2, 6, 8, 5, 3, 7, 9, 1],
+    [7, 1, 3, 9, 2, 4, 8, 5, 6],
+    [9, 6, 1, 5, 3, 7, 2, 8, 4],
+    [2, 8, 7, 4, 1, 9, 6, 3, 5],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+]
+
 t.test(i, True, SudokuSolver().solve(i))
 
 t.status()
