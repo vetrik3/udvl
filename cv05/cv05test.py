5e68145d57b4f3dc859a14b1f873f441a1dcf2f2
Jozef Šiška <siska@ii.fmph.uniba.sk>
2014-03-20 20:39:46 +0100

    cv05: upraveny python test pre python3
    plus nejake dalsie drobne zmeny

--- cv05/cv05test.py
+++ cv05/cv05test.py
@@ -1,8 +1,8 @@
-#!/bin/env python
+#!/bin/env python3
 
 import io
-StringIO = io
 import itertools
+import traceback
 
 """
     Testovaci program pre kniznicu cnf.py
@@ -10,6 +10,18 @@ import itertools
 
 from cnf import VariableMap, Cnf, CnfClause, CnfLit
 
+def ignoreException(func):
+    def wrapper(*args, **kwargs):
+        try:
+            func(*args, **kwargs)
+        except:
+            print('ERROR: %s: Exception raised:\n%s\n%s\n%s' % (
+                func.__name__,
+                '-'*20,
+                traceback.format_exc(),
+                '-'*20)
+            )
+    return wrapper
 
 class Tester(object):
     def __init__(self):
@@ -22,8 +34,8 @@ class Tester(object):
             self.passed += 1
             return True
         else:
-            print("    Failed: %s:" %  msg)
-            print("      got %s  expected %s" % (repr(result), repr(expected)))
+            print("Failed: %s:" %  msg)
+            print("    got %s  expected %s" % (repr(result), repr(expected)))
             print("")
             return False
 
@@ -38,34 +50,40 @@ class Tester(object):
     def check(self, b):
         self.state = self.state and b
 
+    @ignoreException
     def testCnfWrite(self, cnf, varMap, dimacs):
-        oF = StringIO.StringIO()
+        oF = io.StringIO()
         cnf.writeToFile(oF, varMap)
         oD = [int(x) for x in oF.getvalue().split()]
         self.compare(oD, dimacs, 'Cnf.writeToFile %s' % cnf.toString())
 
+    @ignoreException
     def testCnfRead(self, dimacs, varMap, ecnf):
-        iF = StringIO.StringIO(dimacs)
+        iF = io.StringIO(dimacs)
         cnf = Cnf.readFromFile(iF, varMap)
         self.compare(cnf.toString(), ecnf.toString(), 'Cnf.readFromFile %s' % ecnf.toString())
 
+    @ignoreException
     def testCnfClauseWrite(self, cnf, varMap, dimacs):
-        oF = StringIO.StringIO()
+        oF = io.StringIO()
         cnf.writeToFile(oF, varMap)
         oD = [int(x) for x in oF.getvalue().split()]
         self.compare(oD, dimacs, 'CnfClause.writeToFile %s' % cnf.toString())
 
+    @ignoreException
     def testCnfLitWrite(self, cnf, varMap, dimacs):
-        oF = StringIO.StringIO()
+        oF = io.StringIO()
         cnf.writeToFile(oF, varMap)
         oD = [int(x) for x in oF.getvalue().split()]
         self.compare(oD, dimacs, 'CnfLit.writeToFile %s' % cnf.toString())
 
+    @ignoreException
     def testCnfClauseRead(self, dimacs, varMap, ecnf):
-        iF = StringIO.StringIO(dimacs)
+        iF = io.StringIO(dimacs)
         cnf = CnfClause.readFromFile(iF, varMap)
         self.compare(cnf.toString(), ecnf.toString(), 'CnfClause.readFromFile %s' % ecnf.toString())
 
+    @ignoreException
     def testVarMap(self, namesPre, namesAdd):
         varMap = VariableMap(namesPre)
         for name in namesAdd:
@@ -84,15 +102,16 @@ class Tester(object):
                 sorted([ (rev[k],k) for k in rev]),
                 'VariableMap: reverse() %s' % varMap.toString())
 
-        oF = StringIO.StringIO()
+        oF = io.StringIO()
         varMap.writeToFile(oF)
-        iF = StringIO.StringIO(oF.getvalue())
+        iF = io.StringIO(oF.getvalue())
         vm = VariableMap.readFromFile(iF)
         self.compare(
-                sorted([ (k,vm[k]) for k in vm]),
-                sorted([ (k,varMap[k]) for k in varMap]),
+                sorted([ (k,vm[k]) for k in vm.keys()]),
+                sorted([ (k,varMap[k]) for k in varMap.keys()]),
                 'VariableMap read/write %s (%s)' % (varMap.toString(), repr(oF.getvalue())))
 
+    @ignoreException
     def testExtendVarMap(self, cnf, names):
         for pre in range(len(names)):
             varMap = VariableMap()
@@ -106,6 +125,7 @@ class Tester(object):
             for k,v in [ (v,varMap[v]) for v in names[:pre]]:
                 self.compare(varMap[k], v, 'extendVarMap fixed mismatch %s %s' % (names, cnf.toString()))
 
+    @ignoreException
     def testCnf(self, cnf, string, cases):
         self.compare(cnf.toString(), string, 'toString')
 
@@ -116,20 +136,9 @@ class Tester(object):
 t = Tester()
 
 try:
-    for p in itertools.permutations(['a', 'b', 'c', 'd']):
-        for l in range(3):
-            t.testVarMap(p[:l], p[l:])
-
-    t.testExtendVarMap(
-            Cnf( [
-                CnfClause( [ CnfLit('a'), CnfLit.Not('b') ] ),
-                CnfClause( [ ] ),
-                CnfClause( [ CnfLit('x') ] ),
-                CnfClause( [ CnfLit('c'), CnfLit.Not('c'), CnfLit('c', True) ] ),
-                ]),
-            ['a', 'b', 'c', 'x'],
-            )
-
+    #
+    # Jednoduche testy literalov
+    #
     t.compare( CnfLit('a').name, 'a', 'CnfLit.name' )
     t.compare( CnfLit('a').neg, False, 'CnfLit.neg' )
     t.compare( CnfLit.Not('a').name, 'a', 'CnfLit.Not.name' )
@@ -139,18 +148,15 @@ try:
     t.compare( (--CnfLit('a')).name, 'a', '--CnfLit.Not.name' )
     t.compare( (--CnfLit('a')).neg, False, '--CnfLit.Not.neg' )
 
+
+    #
+    # testy toString, eval pre CnfLit, CnfClause, Cnf
+    #
     t.testCnf(
             CnfLit('a'), 'a',
             [
                 ({'a':True}, True),
                 ({'a':False}, False),
-            ])
-
-    t.testCnf(
-            CnfLit('a', True), '-a',
-            [
-                ({'a':True}, False),
-                ({'a':False}, True),
             ])
 
     t.testCnf(
@@ -187,6 +193,34 @@ try:
             ])
 
 
+    #
+    # testy pre VariableMap
+    #
+    varMap = VariableMap()
+    varMap.addVar('z').addVar('y').addVar('x')
+    t.compare( varMap.get('y'), 2, 'varMap.get(y)')
+    t.compare( varMap['y'], 2, 'varMap[y]')
+
+    for p in itertools.permutations(['a', 'b', 'x']):
+        for l in range(3):
+            t.testVarMap(p[:l], p[l:])
+
+    #
+    # testy extendVarMap
+    #
+    t.testExtendVarMap(
+            Cnf( [
+                CnfClause( [ CnfLit('a'), CnfLit.Not('b') ] ),
+                CnfClause( [ ] ),
+                CnfClause( [ CnfLit('x') ] ),
+                CnfClause( [ CnfLit('c'), CnfLit.Not('c'), -CnfLit('c') ] ),
+                ]),
+            ['a', 'b', 'c', 'x'],
+            )
+
+    #
+    # testy readFrom / writeToFile
+    #
     t.testCnfLitWrite(
             CnfLit('a'),
             VariableMap().addVar('a'),
@@ -200,7 +234,7 @@ try:
     t.testCnfClauseRead(
             '1 -2 3 0\n',
             VariableMap().addVar('c').addVar('b').addVar('a'),
-            CnfClause( [ CnfLit('c'), CnfLit('b',True), CnfLit('a') ] )
+            CnfClause( [ CnfLit('c'), CnfLit.Not('b'), CnfLit('a') ] )
             )
 
     t.testCnfWrite(
@@ -221,6 +255,8 @@ try:
                 CnfClause( [ ] ),
                 CnfClause( [ CnfLit('a'), CnfLit('c') ] ),
                 ]))
+
+    print("END")
 
 finally:
     t.status()
